a12d043ed3ff8122957954bb771f0f408c32034c
Kentaro Furukawa <info@ken89.com>
2023-10-03 21:04:42 +0900

    change port to PORT, add dotenv dependency, but not using it yet.

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.sendStatus(200).send('express server!');
 })
 
-app.listen(3000, () => {
-  console.log('server started on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
